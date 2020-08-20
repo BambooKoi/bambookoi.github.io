0070a5ef400e4d710ca071ce415da162a83841d9
BambooKoi <barberry.koi@gmail.com>
2020-08-20 00:36:37 -0400

    Added new index.html
    - Previous index.html is now links.html
        - New home page in progress
    - Updated product images with shop logo in corner
    - Added bulk discount information for RedBubble

--- script.js
+++ script.js
@@ -9,15 +9,31 @@ gtag('config', 'UA-102814295-6');
 window.onscroll = function() {scrolling()};
         
 function scrolling() {
-if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("scrolled").innerHTML = '<a href="#" id="scrolled" title="Back to top"><i class="las la-angle-up"></i></a>';
-} else {
-    document.getElementById("scrolled").innerHTML = '';
-}
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("scrolled").innerHTML = '<a href="#" id="scrolled" title="Back to top"><i class="las la-angle-up"></i></a>';
+  } else {
+      document.getElementById("scrolled").innerHTML = '';
+  }
 }
 
 // Import header.html, nav.html and footer.html
 $(function() {
   $("#header").load("header.html");
+  // $("#nav-js").load("nav.html");
   $("#footer").load("footer.html");
 });
+
+/*
+// nav toggle
+function navToggle() {
+  var x = document.getElementById("nav-js");
+  if (x.style.display === "inline-block") {
+    x.style.display = "none";
+    document.getElementByClassName("burger").innerHTML = '<i class="las la-bars"></i>';
+  } else {
+    x.style.display = "inline-block";
+    // Toggle icon: X doesn't appear?
+    document.getElementByClassName("burger").innerHTML = '<i class="las la-times"></i>';
+  }
+}
+*/
