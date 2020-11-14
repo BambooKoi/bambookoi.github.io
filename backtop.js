10acf87cd7f1c26f7e546b965e37b4c611148215
BambooKoi <barberry.koi@gmail.com>
2020-11-14 17:04:43 -0500

    JS tweaks

---
+++ backtop.js
@@ -0,0 +1,17 @@
+// Show Back to Top on Scroll:
+var scrollup = document.getElementById("backtop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollup.style.display = "block";
+  } else {
+    scrollup.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
--- script.js
+++ script.js
@@ -14,25 +14,6 @@ $(function() {
 });
 
 
-// Show Back to Top on Scroll:
-var scrollup = document.getElementById("backtop");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    scrollup.style.display = "block";
-  } else {
-    scrollup.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-
 // Fix Navigation Menu Width if Switched between <= 1080px:
 // media query event handler
 if (matchMedia) {
